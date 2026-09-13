bf3412f146b9e0162f29386c488436a717ff72c0
alititi-jackie <fengjiancheng8@gmail.com>
2026-09-13 13:45:06 -0400

    Create complete English map for all shared core questions

---
+++ lib/shared-core-english-complete.ts
@@ -0,0 +1,3 @@
+import { californiaEnglishCore } from './california-english-core'
+import { sharedCoreEnglish } from './shared-core-english'
+export const sharedCoreEnglishComplete={...californiaEnglishCore,...sharedCoreEnglish}
