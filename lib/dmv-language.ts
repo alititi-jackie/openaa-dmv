ddc2d5b48d0559ed63cebcddffef090f72b957ef
alititi-jackie <fengjiancheng8@gmail.com>
2026-09-14 22:30:37 -0400

    feat: wire New York English translations into language system

--- lib/dmv-language.ts
+++ lib/dmv-language.ts
@@ -4,6 +4,7 @@ import { californiaCoreEnglishById } from './california-english-core'
 import { californiaExpandedEnglishById } from './california-english-expanded'
 import { californiaQualityEnglishById } from './california-english-quality'
 import { newJerseyEnglishById } from './new-jersey-english'
+import { newYorkEnglishById } from './new-york-english'
 import { pennsylvaniaEnglishById } from './pennsylvania-english'
 import { getQuestionSourceForLanguage } from './question-bank'
 import { sharedCoreEnglish } from './shared-core-english'
@@ -16,7 +17,8 @@ export type BilingualDmvQuestion = DmvQuestion & { en?: DmvEnglishContent; keywo
 export function languageStorageKey(stateSlug: string) { return `openaa-dmv:${stateSlug}:language` }
 
 function externalEnglish(question: DmvQuestion) {
-  return pennsylvaniaEnglishById[question.id]
+  return newYorkEnglishById[question.id]
+    ?? pennsylvaniaEnglishById[question.id]
     ?? newJerseyEnglishById[question.id]
     ?? californiaQualityEnglishById[question.id]
     ?? californiaEnglishById[question.id]
